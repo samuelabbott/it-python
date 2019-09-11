02d4ab73b6b4e5998ec178a1e967eb725dc61f0e
Samuel Abbott <SamuelAbbott@protonmail.com>
2019-09-10 21:23:18 -0400

    Initial Commit

---
+++ banner.py
@@ -0,0 +1,16 @@
+def banner(title, author):
+    title_length = len(title)
+    byline = f"By {author}"
+    byline_length = len(byline)
+    banner_length = max(title_length, byline_length) + 8
+    print("=" * banner_length)
+    print(f"{title:^{banner_length}}")
+    print(f"{byline:^{banner_length}}")
+    print(f"{'=' * banner_length}")
+    print("")
+
+
+banner("BANNER", "Samuel")
+name = input("What is your name? ")
+title = input("What is your quest? ")
+banner(title, author)
